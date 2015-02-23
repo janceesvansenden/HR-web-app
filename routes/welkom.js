5ee9ac77546e6c19371e8110610d60ac16a5504d
Bart Keulen <bart_keulen@hotmail.com>
2015-02-23 10:49:44 +0100

    Verwijderen oude bestanden

--- routes/welkom.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var router = express.Router();
-
-/* GET /default listing. */
-router.get('/', function(req, res, next) {
-  res.send('Hallo Bart');
-});
-
-module.exports = router;
