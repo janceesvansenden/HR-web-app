7d5eac22bf931147baeb3893f6b70581c6c271de
JanCees van Senden <jancees.van.senden@gmail.com>
2015-02-23 15:16:52 +0100

    Added router to server

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
-var express = require('express')
-var path = require('path')
-var app = express()
+var express = require('express');
+var path = require('path');
+var app = express();
 
 // View engine setup
 app.engine('html', require('ejs').renderFile);
@@ -10,17 +10,21 @@ app.set('view engine', 'html');
 app.use("/views", express.static(__dirname + "/views"));
 app.use("/assets", express.static(__dirname + "/assets"));
 
+// Get an instance of router
+var router = express.Router();
+
 // Render index.html
-app.get('/', function (req, res) {
+router.get('/', function (req, res) {
   res.render('index', { title: 'De Nieuwe Rekening' });
 });
 
+// apply router to application
+app.use('/', router);
+
 // Create server
 var server = app.listen(3000, function () {
-
   var host = server.address().address
   var port = server.address().port
-
+  
   console.log('App listening at http://%s:%s', host, port)
-
 })
