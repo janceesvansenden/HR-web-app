9033bff8d6e59da1a4434f1e096ca6788f2bfabf
JanCees van Senden <jancees.van.senden@gmail.com>
2015-03-16 17:16:15 +0100

    minor changes to make it work

--- views/personal/personal.controller.js
+++ views/personal/personal.controller.js
@@ -3,9 +3,9 @@ angular.module('DeNieuweRekening')
     .controller('PersonalController', ['$scope', '$http', function ($scope, $http) {
     	$http.get('/personal').
 	    	success(function(data){
-	    		console.log('succes:', data);
+	    		console.log('succes: ', data);
 	    	}).
 	    	error(function(data, status){
-	    		console.log(data, status);
+	    		console.log('error: ',data, status);
 	    	});
     }]);
