493c8c6111103d9c0a641cc2b5d29772ed7a4c80
JanCees van Senden <jancees.van.senden@gmail.com>
2015-03-24 17:19:16 +0100

    Created basic personal page

--- assets/js/angular_app.js
+++ assets/js/angular_app.js
@@ -64,10 +64,12 @@ angular.module('DeNieuweRekening')
     .controller('PersonalController', ['$scope', '$http', function ($scope, $http) {
     	$http.get('/personal').
 	    	success(function(data){
-	    		console.log('succes:', data);
+	    		//console.log('succes: ', data);
+	    		$scope.personal = data;
+	    		console.log($scope.personal);
 	    	}).
 	    	error(function(data, status){
-	    		console.log(data, status);
+	    		console.log('error: ',data, status);
 	    	});
     }]);
 angular.module('DeNieuweRekening')
