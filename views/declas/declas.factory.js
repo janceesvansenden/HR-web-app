dfb70e2448b14d2b4a31a9818664cc0e4fe5b4ac
Bart Keulen <bart_keulen@hotmail.com>
2015-04-02 18:15:49 +0200

    Begin gek te worden van SourceTree

--- views/declas/declas.factory.js
+++ views/declas/declas.factory.js
@@ -2,9 +2,9 @@ angular.module('DeNieuweRekening')
 
 	.factory('deelnemers', function() {
 		return deelnemers = [
-			{ id: 1, name: 'Bart', value: 1 },
-			{ id: 2, name: 'Jan Cees', value: 1 },
-			{ id: 3, name: 'Henk', value: 1 },
-			{ id: 4, name: 'Frits', value: 1 }
+			{ id: 1, name: 'Bart', kosten: '0.00', value: 1 },
+			{ id: 2, name: 'Jan Cees', kosten: '0.00', value: 1 },
+			{ id: 3, name: 'Henk', kosten: '0.00', value: 1 },
+			{ id: 4, name: 'Frits', kosten: '0.00', value: 1 }
 		];
 	});
